8f0080eaf98febd371f685215b4c25c3291bf8d8
Twinparadox <nww731@gmail.com>
2018-04-04 22:09:17 +0900

    단어 추가 구현

--- WordMaster/WordMaster/AddWordWindow.xaml.cs
+++ WordMaster/WordMaster/AddWordWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,16 @@ namespace WordMaster
     {
         private string defaultTextboxEng = "영단어를 입력해주세요.";
         private string defaultTextboxKor = "단어의 뜻을 입력해주세요.";
+
+        /* Reading File and make string */
+        private string[] enText;
+        private string[] koText;
+
+        /* line */
+        private int lineCount;
+
+        /* is already? */
+        private bool isAlready;
 
         public AddWordWindow()
         {
@@ -77,6 +88,47 @@ namespace WordMaster
             {
                 MessageBox.Show("단어의 뜻을 입력해주세요.");
                 return;
+            }
+            try
+            {
+                string[] enText = File.ReadAllLines(WordBook.saveEnPath, Encoding.GetEncoding("utf-8"));
+                string[] koText = File.ReadAllLines(WordBook.saveKrPath, Encoding.GetEncoding("utf-8"));
+                lineCount = Properties.Settings.Default.curSize;
+            }
+            catch (Exception ex)
+            {
+            }
+
+            isAlready = false;
+            TextBoxEnglishWord.Focus();
+            string enWord = this.TextBoxEnglishWord.Text;
+            string koWord = this.TextBoxKoreanWord.Text;
+            string count = ",0,0";
+
+            if (enWord != "" && koWord != "")
+            {
+                for (int i = 0; i < lineCount; i++)
+                {
+                    if (enText[i] == enWord + count)
+                        isAlready = true;
+                }
+                if (isAlready == true)
+                {
+                    MessageBox.Show("이미 입력된 단어입니다.", "알림", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    try
+                    {
+                        File.AppendAllText(WordBook.saveEnPath, enWord + count + "\r\n", Encoding.UTF8);
+                        File.AppendAllText(WordBook.saveKrPath, koWord + "\r\n", Encoding.UTF8);
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                this.TextBoxKoreanWord.Text = defaultTextboxKor;
+                this.TextBoxEnglishWord.Text = defaultTextboxEng;
             }
         }
     }
